f2b016672ad28852234ab0976f7b1553c728ba6b
iancho <ian.j.cho@gmail.com>
2019-07-05 21:18:07 -0700

    Updates

---
+++ precache-manifest.fd387aea7c120bc8f9043781c2fd0859.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf59ad1d712b30a6e6afaac90d02335c",
+    "url": "/barkadawebsite/index.html"
+  },
+  {
+    "revision": "dd8d72b49f9076438080",
+    "url": "/barkadawebsite/static/css/main.facc27bd.chunk.css"
+  },
+  {
+    "revision": "1cbdf0208892e1181ed5",
+    "url": "/barkadawebsite/static/js/2.dd2bb267.chunk.js"
+  },
+  {
+    "revision": "dd8d72b49f9076438080",
+    "url": "/barkadawebsite/static/js/main.6bb92216.chunk.js"
+  },
+  {
+    "revision": "230a4597eec1da1b5e57",
+    "url": "/barkadawebsite/static/js/runtime~main.18f66382.js"
+  },
+  {
+    "revision": "f33822b0f60b4288f6952cdb713f9555",
+    "url": "/barkadawebsite/static/media/banner.f33822b0.png"
+  },
+  {
+    "revision": "3197f49eea407d62ee67d875ea5665fd",
+    "url": "/barkadawebsite/static/media/logo.3197f49e.png"
+  },
+  {
+    "revision": "92e247f373b0d4ce96151a7e8e8f34fd",
+    "url": "/barkadawebsite/static/media/logo1.92e247f3.png"
+  },
+  {
+    "revision": "a0f4ef7d91bc2ea91ad7b0df2ed293dc",
+    "url": "/barkadawebsite/static/media/maria_clara.a0f4ef7d.jpg"
+  },
+  {
+    "revision": "7d3fde9a0c3a1914630cbbae2a23f946",
+    "url": "/barkadawebsite/static/media/maria_clara1.7d3fde9a.jpg"
+  },
+  {
+    "revision": "14f465fd3ff53f5bdd2a57970a636954",
+    "url": "/barkadawebsite/static/media/modern.14f465fd.jpg"
+  },
+  {
+    "revision": "c1460b262a13c0616e8dc23ac8ef837e",
+    "url": "/barkadawebsite/static/media/mountain.c1460b26.jpg"
+  },
+  {
+    "revision": "83c3bece45330a8d06026ca811ccf947",
+    "url": "/barkadawebsite/static/media/mountain1.83c3bece.jpg"
+  },
+  {
+    "revision": "762ff942626240c085afc4405bd20f81",
+    "url": "/barkadawebsite/static/media/muslim.762ff942.jpg"
+  },
+  {
+    "revision": "3ebd73943e82c6a8d5d9e18a66822fbb",
+    "url": "/barkadawebsite/static/media/muslim1.3ebd7394.jpg"
+  },
+  {
+    "revision": "8e6dd84e5a11b2dd95e38ff5a3e16298",
+    "url": "/barkadawebsite/static/media/muslim2.8e6dd84e.jpg"
+  },
+  {
+    "revision": "840537300cede16963f4cfb952cfe05a",
+    "url": "/barkadawebsite/static/media/rural.84053730.jpg"
+  },
+  {
+    "revision": "6a91617663f6b5276c52c9748df3d30e",
+    "url": "/barkadawebsite/static/media/suite_banner.6a916176.jpg"
+  },
+  {
+    "revision": "267415028547b4feed42bedd4ecb8d5d",
+    "url": "/barkadawebsite/static/media/tribal.26741502.jpg"
+  }
+]);
